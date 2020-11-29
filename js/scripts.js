5880eed18d97b3f61370bb107603da5a3285fb33
fria-elenaliu <fria-elenaliu@users.noreply.github.com>
2020-11-28 16:08:27 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+alert('Hello World');
+
+let testMathFunction=Math.round(3.68);
+document.getElementById("roundingResult").value = testMathFunction;
+
+let favoriteFood='Kiwi';
+document.write(favoriteFood);
