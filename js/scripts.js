c3ea1a6f8cda035eeb66e21a56ce7b76c9b8911c
fria-elenaliu <fria-elenaliu@users.noreply.github.com>
2020-12-02 13:13:42 -0800

    Update scripts.js
    Redefine the variable "pokemonList" to associate the name, type and height within the same array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-let pokemonList = [];
-pokemonList.name = ['Bulbasaur', 'Charmander', 'Nidoran', 'Vulpix', 'Jigglypuff'];
-pokemonList.height= [0.7, 0.6, 0.4, 0.6, 0.5 ];
-pokemonList.type= ['grass', 'fire', 'poison', 'fire', 'fairy'];
+/*let pokemonList1 = [];
+pokemonList1.name = ['Bulbasaur', 'Charmander', 'Nidoran', 'Vulpix', 'Jigglypuff'];
+pokemonList1.height= [0.7, 0.6, 0.4, 0.6, 0.5 ];
+pokemonList1.type= ['grass', 'fire', 'poison', 'fire', 'fairy'];*/
 
-document.write(pokemonList.name);
-
-
+let pokemonList
+pokemonList = [	{name:'Bulbasaur ', height: 0.7, type: 'grass '},
+				{name:'Charmander ', height: 0.6, type: 'fire '},
+				{name:'Nidoran ', height: 0.4, type: 'poison '},
+				{name:'Vulpix ', height: 0.6, type: 'fire '},
+				{name:'Gigglypuff ', height: 0.5, type: 'fairy '}
+			];
+document.write(pokemonList[1].name,pokemonList[2].name,pokemonList[3].name);
+document.write(pokemonList[1].type,pokemonList[2].type,pokemonList[3].type);
