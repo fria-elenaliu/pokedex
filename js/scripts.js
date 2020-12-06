c5afdeaa65c2fd32a0a49fc4f82d5f2785522f19
fria-elenaliu <fria-elenaliu@users.noreply.github.com>
2020-12-06 10:56:16 -0800

    Adding a loop
    Adding some loop and conditional exercise

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,24 @@ pokemonList1.name = ['Bulbasaur', 'Charmander', 'Nidoran', 'Vulpix', 'Jigglypuff
 pokemonList1.height= [0.7, 0.6, 0.4, 0.6, 0.5 ];
 pokemonList1.type= ['grass', 'fire', 'poison', 'fire', 'fairy'];*/
 
-let pokemonList
-pokemonList = [	{name:'Bulbasaur ', height: 0.7, type: 'grass '},
-				{name:'Charmander ', height: 0.6, type: 'fire '},
-				{name:'Nidoran ', height: 0.4, type: 'poison '},
-				{name:'Vulpix ', height: 0.6, type: 'fire '},
-				{name:'Gigglypuff ', height: 0.5, type: 'fairy '}
-			];
-document.write(pokemonList[1].name,pokemonList[2].name,pokemonList[3].name);
-document.write(pokemonList[1].type,pokemonList[2].type,pokemonList[3].type);
+let pokemonList = [	
+	{name:'Bulbasaur', height: 0.7, type: 'grass'},
+	{name:'Charmander', height: 0.6, type: 'fire'},
+	{name:'Nidoran', height: 0.4, type: 'poison'},
+	{name:'Vulpix', height: 0.6, type: 'fire'},
+	{name:'Gigglypuff', height: 0.5, type: 'fairy'},
+	{name:'Raichu', height: 2.07, type:'electric'}
+	];
+
+let i=0;
+
+ for (let i=0; i<pokemonList.length; i++){
+	if (pokemonList[i].height >1.0){
+		document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'+' - Wow, that\'s big! </li>');
+	}else if(pokemonList[i].height <0.6){
+		document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'+' - Ohh, that\'s small! </li>');
+	}else{
+ 	document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </li>');
+ 	}
+ }
+
