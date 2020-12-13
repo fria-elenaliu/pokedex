c4e725809961af91fc72b80d47cd18395d85fe44
fria-elenaliu <fria-elenaliu@users.noreply.github.com>
2020-12-12 22:37:13 -0800

    Update scripts.js
    Using IIFE to set up the pokemonList respository so that the pokemonList is a local variable.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,35 @@ pokemonList1.name = ['Bulbasaur', 'Charmander', 'Nidoran', 'Vulpix', 'Jigglypuff
 pokemonList1.height= [0.7, 0.6, 0.4, 0.6, 0.5 ];
 pokemonList1.type= ['grass', 'fire', 'poison', 'fire', 'fairy'];*/
 
-let pokemonList = [	
-	{name:'Bulbasaur', height: 0.7, type: 'grass'},
-	{name:'Charmander', height: 0.6, type: 'fire'},
-	{name:'Nidoran', height: 0.4, type: 'poison'},
-	{name:'Vulpix', height: 0.6, type: 'fire'},
-	{name:'Gigglypuff', height: 0.5, type: 'fairy'},
-	{name:'Raichu', height: 2.07, type:'electric'},
-	{name:'Pachirisu', height: 0.4, type:'electric'},
-	//{name:'Buneary', height: 1.4, type:'normal'}
+let pokemonRepository = (function(){
+
+	let pokemonList = [	
+		{name:'Bulbasaur', height: 0.7, type: 'grass'},
+		{name:'Charmander', height: 0.6, type: 'fire'},
+		{name:'Nidoran', height: 0.4, type: 'poison'},
+		{name:'Vulpix', height: 0.6, type: 'fire'},
+		{name:'Gigglypuff', height: 0.5, type: 'fairy'},
+		{name:'Raichu', height: 2.07, type:'electric'},
+		{name:'Pachirisu', height: 0.4, type:'electric'},
 	];
 
-pokemonList.forEach(function(pokemon){
+	function getAll(){
+  		return pokemonList;
+  	}
+
+  	function add(pokemon){
+  		pokemonList.push(pokemon);
+  	}
+
+  	return{
+  		getAll: getAll,
+  		add: add
+  	};
+  })();
+
+pokemonRepository.add({name:'Buneary', height: 1.4, type:'normal'})
+
+pokemonRepository.getAll().forEach(function(pokemon){
 	if (pokemon.height >1.0){
 		document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')'+' - Wow, that\'s big! </li>');
 	}else if(pokemon.height <0.5){
